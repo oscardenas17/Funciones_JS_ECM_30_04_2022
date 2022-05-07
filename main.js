35ecccb0a148d9dcf4f317dfd8ded5bdf5b5db4d
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-07 16:36:58 -0500

    Callbacks

--- main.js
+++ main.js
@@ -11,5 +11,16 @@
 // console.log(saludo());
 
 //Function mediante constructor de objeto
-let saludar = new Function('nombre=`Draven`','return `Hola Usuario ${nombre} como estas`');
-console.log(saludar());
+// let saludar = new Function('nombre=`Draven`','return `Hola Usuario ${nombre} como estas`');
+// console.log(saludar());
+
+//funciones callback o retrollamadas
+let fnA = function(){
+    return `Hola soy la funcion A y me llamaran en B`;
+}
+let fnB = function(callback){
+    console.log(callback());
+    return `Hola soy la funcion B `;
+}
+
+console.log(fnB(fnA));
