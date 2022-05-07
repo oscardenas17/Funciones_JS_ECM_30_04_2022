93be696fb591bae6a58952efca81582288d7bbd3
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-07 16:32:47 -0500

    funcion mediante expresion

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-console.log(saludar());
+// console.log(saludar());
 
-function saludar(nombre=`Marcos`){
-    return `Hola usuario ${nombre} como estas :)`;
-}
- 
+// function saludar(nombre=`Marcos`){
+//     return `Hola usuario ${nombre} como estas :)`;
+// }
+
+//funcion mediante expresion
+const saludo = function (nombre = "Draven") {
+  return `Hola usuario ${nombre} como estas`;
+};
+console.log(saludo());
