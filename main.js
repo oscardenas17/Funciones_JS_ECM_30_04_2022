7f8f527f07a74e151fd3e0a93e0dea9a7a649b6e
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-07 16:40:59 -0500

    Actividad de las Funciones

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// Crear un formulario con mínimo 10 campos de datos obtener 
+// la información y por medio de un botón ejecutar un función 
+// que muestre los datos capturados
+
+const obtenerData= () => {
+    
+    
+    return 'hola'
+}
+
+
+
+
+
 // console.log(saludar());
 
 // function saludar(nombre=`Marcos`){
@@ -14,13 +28,13 @@
 // let saludar = new Function('nombre=`Draven`','return `Hola Usuario ${nombre} como estas`');
 // console.log(saludar());
 
-//funciones callback o retrollamadas
-let fnA = function(){
-    return `Hola soy la funcion A y me llamaran en B`;
-}
-let fnB = function(callback){
-    console.log(callback());
-    return `Hola soy la funcion B `;
-}
+// //funciones callback o retrollamadas
+// let fnA = function(){
+//     return `Hola soy la funcion A y me llamaran en B`;
+// }
+// let fnB = function(callback){
+//     console.log(callback());
+//     return `Hola soy la funcion B `;
+// }
 
-console.log(fnB(fnA));
+// console.log(fnB(fnA));
